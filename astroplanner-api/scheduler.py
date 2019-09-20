1657b381906a8d11d65ed827c2e018db351142df
Jonathan Wong <chi.jonathanwong@gmail.com>
2019-09-20 14:18:09 -0700

    Add flask boilerplate

--- astroplanner-api/scheduler.py
+++ astroplanner-api/scheduler.py
@@ -1,2 +1,6 @@
-import astroplan
-astroplan.test()
+from astropy.coordinates import SkyCoord
+from astroplan import FixedTarget
+
+coordinates = SkyCoord('19h50m47.6s', '+08d52m12.0s', frame='icrs')
+altair = FixedTarget(name='Altair', coord=coordinates)
+
