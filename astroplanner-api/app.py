20f471eeb08ae4592dc1ec45d4dc2236616cad0d
Jonathan Wong <chi.jonathanwong@gmail.com>
2019-09-23 21:53:29 -0700

    Add package boilerplate

--- astroplanner-api/app.py
+++ astroplanner-api/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
+import scheduler
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/add-targets')
+def add_targets():
+    return "Target added"
+
 
 if __name__ == '__main__':
     app.run()
--- astroplanner-api/scheduler.py
+++ astroplanner-api/scheduler.py
@@ -4,3 +4,11 @@ from astroplan import FixedTarget
 coordinates = SkyCoord('19h50m47.6s', '+08d52m12.0s', frame='icrs')
 altair = FixedTarget(name='Altair', coord=coordinates)
 
+
+class Scheduler:
+
+    def __init__(self):
+        self.targets = []
+
+    def get_targets(self):
+        return self.targets 
